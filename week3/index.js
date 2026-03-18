f3b8263fd50e36679d23107fd24c7c5b24fd119c
a513a513 <51474310+a513a513@users.noreply.github.com>
2026-03-18 11:17:45 +0900

    Create index.js

---
+++ week3/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Library from './chapter_03/Library'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Library />
+  </React.StrictMode>
+);
